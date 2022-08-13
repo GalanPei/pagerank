4b8e438e79bd5006e901f639490aa81e50b2ed9f
Galan <galanpei@outlook.com>
2022-08-13 15:39:17 +0800

    Typo fix && refine the implement of Graph

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,9 +1,30 @@
 import numpy as np
+from utils.graph import Graph
 
 
 class PageRank(object):
-    def __init__(self, max_iter: int = 1000) -> None:
+    """
+    Implememt of Page Rank algorithm
+    Args:
+        `graph` (`Graph`): given graph of PageRank
+        `beta`: const used in matrix A
+        `max_iter` (`int`): max iteration steps
+        `epsilon`: threshold used in iteration
+    """
+    def __init__(self, graph: Graph, beta, max_iter: int = 1000, epsilon = 1e-5) -> None:
+        self.graph = graph
+        self.dim = self.graph.dim
+        self.adj_mat = np.array(self.graph.adjacent_matrix)
+        self.degree_mat = np.array(self.graph.degree_matrix)
+        self.M = np.array(self.graph.stochastic_adjacency_matrix)
         self.max_iter = max_iter
+        self.pagerank_vec = np.zeros((self.dim, 1))
+        self.beta = beta
+        self.one_vec = np.ones((self.dim, 1))
+        # Recall that the equation need to solve is r = Ar
+        self.A = self.beta * self.M +\
+            (1 - self.beta) / self.dim * (self.one_vec @ self.one_vec.T)
         
-    def slove(self):
+    def solve(self):
+        pass
         
--- utils/graph.py
+++ utils/graph.py
@@ -3,34 +3,50 @@ from typing import Optional, Union
 
 
 class Graph(object):
-    def __init__(self, vertex: list(str), nodes) -> None:
-        assert(len(vertex) == len(nodes))
+    """
+    A compact implement of graph for given vertex and nodes
+    Args:
+        `nodes`: neighours for each vertex
+        `vertex`: vertex list of graph.
+    """
+
+    def __init__(self, nodes, vertex = None) -> None:
+        assert (len(vertex) == len(nodes))
         self.dim = len(vertex)
         self.vertex = vertex
-        self.nodes = nodes
-        self.index_dict = {self.vertex[i]: i for i in range(len(self.vertex))}
-
-    def __getitem__(self, vertex: Optional[Union[str, int]]):
-        if isinstance(vertex, str):
-            return self.nodes[self.index_dict[vertex]]
-        elif isinstance(vertex, int):
-            return self.nodes[vertex]
+        
+        if isinstance(nodes, dict):
+            self.nodes = nodes
+        elif isinstance(nodes, list):
+            self.nodes = {vertex[i]: nodes[i] for i in range(len(nodes))}
         else:
-            raise TypeError("Wrong type of vertex")
+            raise TypeError("Nodes should be either dict or list")
+
+    def __getitem__(self, vertex):
+        return self.nodes[vertex]
         
     @property
     def adjacent_matrix(self):
         adj_mat = [[0 for _ in range(self.dim)] for _ in range(self.dim)]
         for i in range(self.dim):
-            for neigh in self.ndoes[i]:
+            for neigh in self.nodes[i]:
                 adj_mat[i][neigh] = 1
                 adj_mat[neigh][i] = 1
-                
+
         return adj_mat
-    
+
     @property
     def degree_matrix(self):
         degree_mat = [[0 for _ in range(self.dim)] for _ in range(self.dim)]
         for i in range(self.dim):
             degree_mat[i][i] = len(self.nodes[i])
         return degree_mat
+
+    @property
+    def stochastic_adjacency_matrix(self):
+        sto_adj_mat = [[0 for _ in range(self.dim)] for _ in range(self.dim)]
+        for i in range(self.dim):
+            for j in range(self.dim):
+                if self.adjacent_matrix[i][j]:
+                    sto_adj_mat[i][j] = 1 / self.degree_matrix[j][j]
+        return sto_adj_mat
